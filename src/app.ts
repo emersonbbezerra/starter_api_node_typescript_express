a5a02944a4db99e8cc1340060faf580a9ccc50dd
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-10-19 19:56:06 -0300

    refactor: :recycle: add server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,8 @@
 import express, { Application } from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+export { app };
